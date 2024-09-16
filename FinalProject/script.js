18b5f2891896d2053ddcebfd95c4346ecb462bf5
Cr1m <o_devon2@yahoo.com>
2024-09-16 11:48:36 -0400

    Large Commit, added up to date time and location. Just need to get weather pull request to start working

--- FinalProject/script.js
+++ FinalProject/script.js
@@ -1,3 +1,27 @@
-const d = new Date();
-let time = d.getTime();
-document.getElementById("time").innerHTML = time;
+let time = document.getElementById("currentTime");
+setInterval(() =>{
+    let d = new Date ();
+    time.innerHTML = d.toLocaleTimeString();
+},1000)
+
+let pos = document.getElementById("currentLocation");
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    pos.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+setWeather(getWeather(getLocation()))
+function getWeather(position){
+    var url = 
+        "https://api.openweathermap.org/data/2.5/weather?lat="+ position.coordes.latitude +"&lon="+ position.coordes.longitude + "&appid=92cd8cd8f4fb9e75e5bfd3035db58c91"
+    fetch(url)
+    .then((response) => response.json())
+    .then((date) => console.log(data));
+}
+function setWeather(weather){
+    let test = document.getElementById("currentWeather")
+    test.innerHTML = weather.main.temp
+}
