692da30b2b444a324afde1ef47087a285ed688c5
Cr1m <o_devon2@yahoo.com>
2024-09-16 16:39:25 -0400

    Grabs coords when the weather icon is clicked

--- FinalProject/script.js
+++ FinalProject/script.js
@@ -8,23 +8,25 @@ let pos = document.getElementById("currentLocation");
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
+
   } else {
     pos.innerHTML = "Geolocation is not supported by this browser.";
   }
 }
 
-setWeather(getWeather(getLocation()))
+function showPosition(position){
+    pos.innerHTML = position.coords.latitude + "<br>" + position.coords.longitude
+}
+
 function getWeather(position){
     var url = 
-        "https://api.openweathermap.org/data/2.5/weather?lat="+ position.coordes.latitude +"&lon="+ position.coordes.longitude + "&appid=92cd8cd8f4fb9e75e5bfd3035db58c91"
+        "https://api.openweathermap.org/data/2.5/weather?lat="+ position.coords.latitude +"&lon="+ position.coords.longitude + "&appid=92cd8cd8f4fb9e75e5bfd3035db58c91"
     fetch(url)
     .then((response) => response.json())
     .then((date) => console.log(data));
 }
-function setWeather(){
-    
-}
-function setWeather(weather){
-    let test = document.getElementById("currentWeather")
+
+function showWeather(weather){
+    let test = document.getElementById("temp")
     test.innerHTML = weather.main.temp
 }
