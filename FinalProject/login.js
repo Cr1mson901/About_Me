bf9e78c6b191b5f55edfb653b43c0b6e20298d61
Cr1m <38981037+Cr1mson901@users.noreply.github.com>
2024-09-19 21:01:14 -0400

    Ability to select a profile when logging in

--- FinalProject/login.js
+++ FinalProject/login.js
@@ -1,15 +1,29 @@
 var userNames = JSON.parse(localStorage.getItem("userNames") || '[]');
+var iconSelected = false;
 //Adds all locally stored usernames//
 window.onload = function() {
     userNames.forEach((name) => addUser(name));
-
+    var figures = document.querySelectorAll('figure')
+    for (let figure of figures) {
+        figure.addEventListener("click", function() {
+            iconSelect(this);
+        })
+}
 };
+
 
 // Button implementation for login screen//
 const loginBTN = document.getElementById("loginBTN");
+// InputField for login grabbed
+const inputField = document.getElementById("loginInput");
+inputField.addEventListener('input', function() {
+    if (iconSelected){
+        console.log("reset")        
+        iconReset(false)
+    }
+})
 
 loginBTN.addEventListener("click", function(e){
-    const inputField = document.getElementById("loginInput");
     const name = inputField.value.toLowerCase()
     if (inputField.value == '') {
         console.log('empty')
@@ -34,6 +48,10 @@ function addUser(name) {
     // Caption Element
     const newName = document.createElement('figcaption');
     newName.textContent = name
+
+    newUser.addEventListener("click", function(){
+        iconSelect(this);
+    });
     // Adds elements into the figure
     newUser.appendChild(img)
     newUser.appendChild(newName);
@@ -47,3 +65,20 @@ function addUser(name) {
 function insertAfter(referenceNode, newNode) {
     referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
   }
+
+
+function iconSelect(icon){
+    iconReset(true);
+    let img = icon.querySelector("img");
+    img.src = "./ICON/irix/selectedDisplay.png"
+    inputField.value = icon.querySelector("figcaption").innerHTML;
+}
+
+function iconReset(selected) {
+    var figures = document.querySelectorAll('figure');
+    for (let figure of figures) {
+        let img = figure.querySelector("img");
+        img.src = "./ICON/irix/display.png"
+    }
+    iconSelected = selected
+}
